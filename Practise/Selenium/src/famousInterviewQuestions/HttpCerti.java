5ffbc80b4c1773160322e76f6f3c15d8bca11aa7
Prem212 <rajaprem21@yahoo.com>
2018-07-11 22:39:49 +0530

    Firefoxprofile code

--- Practise/Selenium/src/famousInterviewQuestions/HttpCerti.java
+++ Practise/Selenium/src/famousInterviewQuestions/HttpCerti.java
@@ -1,7 +1,11 @@
 package famousInterviewQuestions;
 
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -21,9 +25,17 @@ public class HttpCerti {
 		c.merge(ch);												//Merging the set capabilities to ur local
 	
 		//	c.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS,true); this line is also possible from options
-	
+
 		
-	
+/*	// Code to set profile for firefox and pass it on to webdriver.	
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("", true);
+		
+		FirefoxOptions options = new FirefoxOptions();
+		options.setProfile(profile);
+		
+		WebDriver driver = new FirefoxDriver(options);
+	*/
 
 	}
 
